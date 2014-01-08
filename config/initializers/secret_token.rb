1764a59e271626651429bc7555f463718f26437f
Sebastian Roldan <sroldan@alumnos.uai.cl>
2014-01-07 23:39:22 -0300

    primergitdevelop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Develop::Application.config.secret_key_base = 'b5ec2e6149d1f21e2075423afcd434eadc8ed951492646c18f72c8c7f0fc392c94524f8ae5f22f85b6ccd27df8aca2ef024a040a83a835d2a549fa5498d62f8c'
